fe1788e7e63888c81f51841bf692eb0ab801b17a
Abi Hafshin Alfarouq <abi@hafs.in>
2016-03-27 23:09:22 +0700

    [wpe] close when webprocess crash

--- gobred/main-wpe.c
+++ gobred/main-wpe.c
@@ -3,15 +3,26 @@
 
 #include <glib.h>
 
+static GMainLoop* loop;
+
 static void
 decidePolicyForNavigationResponse (WKPageRef page, WKNavigationResponseRef res, WKFramePolicyListenerRef listener, WKTypeRef type, const void* data) {
-WKFramePolicyListenerUse(listener);
+	WKFramePolicyListenerUse(listener);
 }
 
 static void
 decidePolicyForNavigationAction (WKPageRef page, WKNavigationActionRef res, WKFramePolicyListenerRef listener, WKTypeRef type, const void* data) {
-WKFramePolicyListenerUse(listener);
+	WKFramePolicyListenerUse(listener);
 }
+
+
+static void
+webProcessDidCrash (WKPageRef oldPage, const void* clientInfo)
+{
+    g_warning("Web process crash!\n");
+    g_main_loop_quit (loop);
+}
+
 
 static WKPageNavigationClientV0 s_navigationClient = {
     { 0, NULL },
@@ -30,7 +41,7 @@ static WKPageNavigationClientV0 s_navigationClient = {
     NULL, // renderingProgressDidChange
     NULL, // canAuthenticateAgainstProtectionSpace
     NULL, // didReceiveAuthenticationChallenge
-    NULL, // webProcessDidCrash
+    webProcessDidCrash, // webProcessDidCrash
     NULL, // copyWebCryptoMasterKey
     NULL, // didBeginNavigationGesture
     NULL, // willEndNavigationGesture
@@ -38,19 +49,34 @@ static WKPageNavigationClientV0 s_navigationClient = {
     NULL, // didRemoveNavigationGestureSnapshot
 };
 
+
 int main(int argc, char* argv[])
 {
-    GMainLoop* loop = g_main_loop_new(NULL, FALSE);
+    loop = g_main_loop_new(NULL, FALSE);
 
     WKContextConfigurationRef contextConfiguration = WKContextConfigurationCreate();
     WKStringRef injectedBundlePath = WKStringCreateWithUTF8CString(PACKAGE_LIB_DIR "/WPEInjectedBundle.so");
     WKContextConfigurationSetInjectedBundlePath(contextConfiguration, injectedBundlePath);
     WKRelease(injectedBundlePath);
 
+    gchar *storagePath = g_build_filename(g_get_user_data_dir(), "gobred/local-storage", NULL);
+    g_mkdir_with_parents (storagePath, 0700);
+    WKStringRef storagePathWK = WKStringCreateWithUTF8CString(storagePath);
+    g_free(storagePath);
+    WKContextConfigurationSetLocalStorageDirectory(contextConfiguration, storagePathWK);
+    WKRelease(storagePathWK);
+
+    gchar *dbPath = g_build_filename(g_get_user_data_dir(), "gobred/database", NULL);
+    g_mkdir_with_parents (dbPath, 0700);
+    WKStringRef dbPathWK = WKStringCreateWithUTF8CString(dbPath);
+    g_free(dbPath);
+    WKContextConfigurationSetIndexedDBDatabaseDirectory(contextConfiguration, dbPathWK);
+    WKRelease(dbPathWK);
+
     WKContextRef context = WKContextCreateWithConfiguration(contextConfiguration);
     WKRelease(contextConfiguration);
 
-    WKStringRef pageGroupIdentifier = WKStringCreateWithUTF8CString("WPEPageGroup");
+    WKStringRef pageGroupIdentifier = WKStringCreateWithUTF8CString("GobredPageGroup");
     WKPageGroupRef pageGroup = WKPageGroupCreateWithIdentifier(pageGroupIdentifier);
     WKRelease(pageGroupIdentifier);
 
@@ -58,7 +84,8 @@ int main(int argc, char* argv[])
     // Allow mixed content.
     WKPreferencesSetAllowRunningOfInsecureContent(preferences, true);
     WKPreferencesSetAllowDisplayOfInsecureContent(preferences, true);
-    WKPreferencesSetLogsPageMessagesToSystemConsoleEnabled(preferences, true);
+    if (!g_getenv("WPE_SHELL_DISABLE_CONSOLE_LOG"))
+      WKPreferencesSetLogsPageMessagesToSystemConsoleEnabled(preferences, true);
     WKPageGroupSetPreferences(pageGroup, preferences);
 
     WKPageConfigurationRef pageConfiguration  = WKPageConfigurationCreate();
@@ -66,7 +93,7 @@ int main(int argc, char* argv[])
     WKPageConfigurationSetPageGroup(pageConfiguration, pageGroup);
 
     if (!!g_getenv("WPE_SHELL_COOKIE_STORAGE")) {
-      gchar *cookieDatabasePath = g_build_filename(g_get_user_cache_dir(), "cookies.db", NULL);
+      gchar *cookieDatabasePath = g_build_filename(g_get_user_data_dir(), "gobred/cookies.db", NULL);
       WKStringRef path = WKStringCreateWithUTF8CString(cookieDatabasePath);
       g_free(cookieDatabasePath);
       WKCookieManagerRef cookieManager = WKContextGetCookieManager(context);
@@ -84,7 +111,7 @@ int main(int argc, char* argv[])
     WKURLRef shellURL = WKURLCreateWithUTF8CString(url);
     WKPageLoadURL(page, shellURL);
     WKRelease(shellURL);
-
+    
     g_main_loop_run(loop);
 
     WKRelease(view);
